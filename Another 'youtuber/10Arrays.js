2e94108f3298b010b68ff7b6be10737ffb78ef96
Souravprajapath <souravprajapath257@gmail.com>
2025-03-27 12:51:00 +0530

    changes

---
+++ Another 'youtuber/10Arrays.js
@@ -0,0 +1,22 @@
+// array
+const students=["sourav", "sangi"]
+const marks=[1,2,3,4,5,6]
+
+//but in js we can add no. array bollean in one
+const myArr=[1,"sou", true]
+
+console.log(students)
+
+/* 1 */students.reverse()
+console.log(students)
+
+/* 2 */ 
+myArr.push("abcd") //add last element
+console.log(myArr)
+
+/* 3 */
+myArr.pop() //remove last element
+console.log(myArr)
+
+/* 4 */
+console.log(students.indexOf("sourav"))//find index of element
